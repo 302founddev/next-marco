929698de8ab496babb43ff78e9bad787e37586a5
NothingTowinDev <nothingtowindev@gmail.com>
2024-09-09 19:55:58 +0200

    add -- Reglas css de tailwinds para el header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,33 @@
+import './styles/tailwinds_styles.css'
+
 const Header = () => {
+  
   return (
-    <section>
-      <h2>Header</h2>
-    </section>
+    
+    <header className='header'>
+      
+      <section>
+        
+        <nav className='nav'>
+          <div>APP LOGO</div>
+          <div>
+            <ul className='menu'>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div>
+            LANGUAJE SWITCHER
+          </div>
+        </nav>
+
+      </section>
+
+    </header>
+    
   )
+
 }
 
 export default Header
